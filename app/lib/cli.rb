cad5b3493630ed593fb650df292e8eb78f6db345
chirptotheeast <alyssa.easterly@yahoo.com>
2020-10-23 12:08:28 -0500

    added welcome art

--- app/lib/cli.rb
+++ app/lib/cli.rb
@@ -20,12 +20,21 @@ end
 
 
 #Evil plan generator artwork
-def render_ascii_art
-    
-    File.readlines("../ascii_art/welcome_art.txt") do |line|
-    puts line
-  end
+
+   def render_ascii_art
+            puts " "
+            puts " "
+            puts " "
+            puts "
+               __        _ _        ___ _                     ___                            _             
+              /____   _ (_| |     / _ | | __ _ _ __          / _ \\  ___ _ __   ___ _ __ __ _| |_ ___   _ __ 
+             /_\\ \\ \\  / | | |    / /_)| |/ _` |    \\        / /_  \\/ _ | '_ \\ / _ | '__/ _` | __/ _ \\ | '__|
+            //__  \\ V / | | |   / ___/| | (_| | || |       / /_ \\ |  __| | | |  __| | | (_| | || (_)  | |   
+            \\__/   \\_/  |_|_|   \\/    |_|\\__,_|_||_|       \\____/  \\___|_| |_|\\___|_|  \\__,_|\\__\\___/ |_|   
+                                                                                     
+            ".red
 end
+
 
 
 
@@ -43,6 +52,7 @@ end
 
 #1.
 def get_user_data
+    render_ascii_art
     begin_generator
     user_data = gets.chomp
    if user_data == '1' 
